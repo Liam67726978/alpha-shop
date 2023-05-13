3a5d5338060ef8ba86fb45b9eec71d4a0da7d72e
Liam <liam67726978@gmail.com>
2023-05-13 20:42:23 +0800

    Changed file paths in src folder to absolute paths

--- src/Components/main/Cart/icons.jsx
+++ src/Components/main/Cart/icons.jsx
@@ -1,5 +1,5 @@
-import { ReactComponent as Minus } from "../../../icons/Minus.svg";
-import { ReactComponent as Plus } from "../../../icons/Plus.svg";
+import { ReactComponent as Minus } from "icons/Minus.svg";
+import { ReactComponent as Plus } from "icons/Plus.svg";
 
 const icons = {
   Minus,
--- src/Components/main/Register/Icons.jsx
+++ src/Components/main/Register/Icons.jsx
@@ -1,9 +1,9 @@
-import { ReactComponent as Step1 } from "../../../icons/StepNumber1.svg";
-import { ReactComponent as Step2 } from "../../../icons/StepNumber2.svg";
-import { ReactComponent as Step3 } from "../../../icons/StepNumber3.svg";
-import { ReactComponent as Triangle } from "../../../icons/Triangle.svg";
-import { ReactComponent as Arrow } from "../../../icons/Arrow.svg";
-import { ReactComponent as Checked } from "../../../icons/Checked.svg";
+import { ReactComponent as Step1 } from "icons/StepNumber1.svg";
+import { ReactComponent as Step2 } from "icons/StepNumber2.svg";
+import { ReactComponent as Step3 } from "icons/StepNumber3.svg";
+import { ReactComponent as Triangle } from "icons/Triangle.svg";
+import { ReactComponent as Arrow } from "icons/Arrow.svg";
+import { ReactComponent as Checked } from "icons/Checked.svg";
 
 const Icons = {
   Step1,
