c8d64e1ac757236c6c24bc9ef02322f80971a562
Liam <liam67726978@gmail.com>
2023-05-14 19:29:20 +0800

    Update total formatting in CartInfo component

--- src/Components/main/Cart/CartInfo.jsx
+++ src/Components/main/Cart/CartInfo.jsx
@@ -1,10 +1,20 @@
 import styles from "./CartInfo.module.scss";
 
 function CartInfo({ data }) {
+  // 計算total
   let total = 0;
   data.forEach((product) => {
     total += product.price * product.quantity;
   });
+  // 設定total格式
+  total = total.toLocaleString("zh-TW", {
+    style: "currency",
+    currency: "TWD",
+    currencyDisplay: "narrowSymbol",
+    maximumFractionDigits: 0,
+    useGrouping: true,
+  });
+
   return (
     <>
       <div className={`${styles.cartInfo} shipping`}>
@@ -13,7 +23,7 @@ function CartInfo({ data }) {
       </div>
       <div className={styles.cartInfo}>
         <div className={`${styles.text}`}>小計</div>
-        <div className={styles.price}>${total}</div>
+        <div className={styles.price}>{total}</div>
       </div>
     </>
   );
